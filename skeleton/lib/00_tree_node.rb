1dcf7cd902983154f079b5c6e8b9b24c2533a231
Nestor Vega <vega.nestor1@gmail.com>
2022-10-21 13:32:42 -0700

    made node

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
-    
+    attr_reader :parent, :children, :value
+    def initialize(n)
+        @parent = nil
+        @children = []
+        @value = n
+    end
+    def parent=(node)
+        @parent = node
+        @parent.children << self
+    end
 end
+
+node1 = PolyTreeNode.new('a')
+node2 = PolyTreeNode.new('b')
+node1.parent=(node2)
