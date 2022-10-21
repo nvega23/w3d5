3e5fce70dfb7960424505557779ebff1e8e7531a
Rex Kho <rexkho510@gmail.com>
2022-10-21 15:11:13 -0700

    Finished phase 3 of tree node

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,13 +1,48 @@
 class PolyTreeNode
     attr_reader :parent, :children, :value
+
     def initialize(n)
         @parent = nil
         @children = []
         @value = n
     end
+
+    
+    
     def parent=(node)
+        # if !self.parent.nil?
+        #     if self.parent.length > 0
+        #         node2 = self.parent
+        #         node2.children.pop
+    
+        #     end
+        # end
+        if !self.parent.nil?
+            self.parent.children.delete(self)
+        end
+
         @parent = node
-        @parent.children << self
+        if !@parent.nil?
+            if !@parent.children.include?(self)
+                @parent.children << self
+            end
+        end
+        
+    end
+
+    def add_child(node)
+        node.parent=(self)
+    end
+
+    def remove_child(node)
+        if !self.children.include?(node)
+            raise 'error'
+        end
+        node.parent=(nil)
+        
+        
+      
+        # 
     end
 end
 
